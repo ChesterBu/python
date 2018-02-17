e98be45692ea77ba10a77bf14b93175a6f7dce65
chester <291164091@qq.com>
2018-02-17 20:09:41 +0800

    add oop and re

---
+++ reg.py
@@ -0,0 +1,16 @@
+import re
+
+string = 'alex36wupeiqi33xialu32'
+
+print(re.search(r'(?P<name>[a-z]+)\d+',string).group('name'))
+
+print(re.search(r'(?P<name>[a-z]+)(?P<age>\d+)',string).group('age'))
+
+
+import hashlib
+
+obj = hashlib.md5('hahah'.encode('utf8'))
+
+obj.update('hello'.encode('utf-8'))
+
+print(obj.hexdigest())
