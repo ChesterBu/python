d7064209f84733ce864d2839293d9e1803e9d9c8
chester <291164091@qq.com>
2018-10-11 00:02:19 +0800

    socket

--- SOCKET/client.py
+++ SOCKET/client.py
@@ -1,7 +1,21 @@
 import socket
-sk = socket.socket()
+# sk = socket.socket()
+# sk.connect(('127.0.0.1', 8080))
+# sk.send(b'hello')
+# ret = sk.recv(1024)
+# print(ret)
+# sk.close()
+
+#print(type('b'.encode('utf-8')))  # <class 'bytes'>
+
+sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sk.connect(('127.0.0.1', 8080))
-sk.send(b'hello')
-ret = sk.recv(1024)
-print(ret)
-sk.close()
+while True:
+    cmd = input('>>')
+    if cmd.strip() == 'q':
+        sk.send(b'q')
+        break
+
+    sk.send(cmd.encode('utf-8'))
+    res = sk.recv(1024)
+    print(res.decode('utf-8'))
--- SOCKET/server.py
+++ SOCKET/server.py
@@ -1,11 +1,30 @@
 import socket
-sk = socket.socket()
+import subprocess
+# sk = socket.socket()
+# sk.bind(('127.0.0.1', 8080))
+# sk.listen()
+# conn,addr = sk.accept()
+# ret = conn.recv(1024)
+# print(ret)
+# conn.send(b'hi')
+# conn.close()
+# sk.close()
+
+sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #传输方式：网络，方式：tcp
 sk.bind(('127.0.0.1', 8080))
 sk.listen()
-conn,addr = sk.accept()
-ret = conn.recv(1024)
-print(ret)
-conn.send(b'hi')
+conn, addr = sk.accept()
+print('客户端', addr)
+while True:
+    cmd = conn.recv(1024)
+    if len(cmd) == 0 or cmd.decode('utf-8') == 'q':
+        break
+    print(cmd)
+    res = subprocess.Popen(cmd.decode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stderr = res.stderr.read()
+    stdout = res.stdout.read()
+    conn.send(stderr)
+    conn.send(stdout)
+    
 conn.close()
 sk.close()
-
